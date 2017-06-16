64d96a5f1a2a5c2c259377da22d6cbf435663662
root <root@iZ2zeipsk8t2mudb50vbv0Z.(none)>
2017-06-17 02:45:05 +0800

    first commit

---
+++ database/migrations/2017_06_16_100000_create_snapshot_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSnapshotTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('snapshot', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('url');
+            $table->string('ymd');
+            $table->string('keyword');
+            $table->string('content');
+            $table->timestamp('created_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('snapshot');
+    }
+}
